2eb0d600bc6b8a5b5d09b15c44bdf84162bdecbd
andi <andres.dazai@outlook.com>
2021-02-28 12:39:59 +0100

    remove unused import on  notifier factory

--- src/Notifier/Notification/Infrastructure/Service/Notifier/NotifierFactory.php
+++ src/Notifier/Notification/Infrastructure/Service/Notifier/NotifierFactory.php
@@ -7,7 +7,6 @@ namespace App\Notifier\Notification\Infrastructure\Service\Notifier;
 use App\Notifier\Notification\Domain\Aggregate\NotificationType;
 use App\Notifier\Notification\Domain\Service\NotifierInterface;
 use App\Notifier\Notification\Infrastructure\Service\Notifier\PHPMailerNotifierService;
-use Psr\Container\ContainerInterface;
 
 final class NotifierFactory
 {
